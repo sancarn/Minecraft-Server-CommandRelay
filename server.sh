06d16e1d2c6da3a578438a3c6184ff28ab305c00
Panda4994 <allid@live.de>
2018-06-10 22:18:42 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-mkfifo mccommands.pipe
-cat mccommands.pipe | java -jar minecraft_server-1.7.3.jar
-ruby server.rb > mccommands.pipe
+
+ruby server.rb | java -jar minecraft_server-1.7.3.jar
